e8c673772d5c77d56a0297a575b8d3a012745fd0
upcteus <u202210059@upc.edu.pe>
2025-02-25 14:12:12 -0500

    oniii-chan

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './auth/login/login.component';
 import {RegisterComponent} from './auth/register/register.component';
 import {AuthGuard} from './auth/auth.guard';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: '**', redirectTo: 'login', pathMatch: 'full'}
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import {
 } from '@coreui/angular';
 import {HeaderComponent} from './components/header/header.component';
 import {FooterComponent} from './components/footer/footer.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 
 @NgModule({
@@ -25,6 +26,7 @@ import {FooterComponent} from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
