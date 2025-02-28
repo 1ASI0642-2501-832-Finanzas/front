1b9747ea66ed8581ba72bd2f82116d795d63eeae
Belen Ramos <129527512+BelenramosR@users.noreply.github.com>
2025-02-28 00:15:15 -0500

    add HomeComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import {RegisterComponent} from './auth/register/register.component';
 import {AuthGuard} from './auth/auth.guard';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+import {HomeComponent} from './components/home/home.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent}
 ];
